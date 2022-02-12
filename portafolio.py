0ea0e6dfe27a870ab19c07d2772fece6b6fc05f4
danielcarias95 <danielcarias95@gmail.com>
2022-02-11 19:16:21 -0600

    Subiendo tarea

---
+++ portafolio.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/servicios")
+def servicios():
+    return render_template("servicios.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+
